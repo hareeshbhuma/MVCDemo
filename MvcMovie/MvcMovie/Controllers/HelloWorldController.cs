c388317637468fb27999c57dcaa75c4822eeb27f
hareeshbhuma <hareeshbhuma@gmail.com>
2020-01-18 13:58:12 -0600

    mvcsample

---
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult  Index()
+        {
+            return View();
+        }
+
+        
+        public String Welcome(String name="Bhuma",int id=1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, Id is: {id}");
+        }
+    }
+}
