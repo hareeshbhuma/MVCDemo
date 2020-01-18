aacabe455b60acc3a200b44676755a74708493f3
hareeshbhuma <hareeshbhuma@gmail.com>
2020-01-18 14:01:16 -0600

    MVCSample

---
+++ MVCSample/MVCSample/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MVCSample.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Route("/myabout")]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        [Route("/mycontact")]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult mypage()
+        {
+
+            StudentEntities studentEntities = new StudentEntities();
+            List<Student> students=studentEntities.Students.ToList();
+
+            return View(students);
+        }
+
+        public ActionResult Success()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+    }
+}
---
+++ MVCSample/MVCSample/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCSample.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float GPA { get; set; }
+    }
+}
